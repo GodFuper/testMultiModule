6ab07344d0404a5f56178df1cdc1dd809308e433
super4121 <super4121@gmail.com>
2023-02-16 15:11:03 +0600

    Initial commit

---
+++ view/src/main/java/com/example/testMultiModule/model/User.java
@@ -0,0 +1,25 @@
+package com.example.testMultiModule.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
